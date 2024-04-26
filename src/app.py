f01629171f13a162a9ffed4a9ee6a11901ab9bca
Paul RIchard <paul.richard@ysocorp.com>
2024-04-26 21:47:31 +0200

    ADD - Connection and creation test table

--- src/app.py
+++ src/app.py
@@ -1,1 +1,31 @@
-print("Bonjour, ceci est un test de mon application de gestion de mots de passe !")
+import psycopg2
+
+
+mydb = psycopg2.connect(
+  host="db",
+  user="paul",
+  password="paul",
+  database="GestionnaireMDP"
+)
+
+
+cur = mydb.cursor()
+
+# Requête SQL pour créer une table
+create_table_query = '''
+    CREATE TABLE IF NOT EXISTS test_table (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        age INT
+    )
+'''
+
+# Exécution de la requête SQL
+cur.execute(create_table_query)
+
+# Validation des changements dans la base de données
+mydb.commit()
+
+# Fermeture du curseur et de la connexion
+cur.close()
+mydb.close()
